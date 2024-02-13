84a8ceefc1bc778f9f03046856814dd6ba79b680
luciasnow <156396387+luciasnow@users.noreply.github.com>
2024-02-12 22:43:36 -0700

    Update js link

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 $("#logo").hover (function(){
     $("#logo").animate({
-      height: '+=15px',
-      width: '+=15px'
+      height: "+=15px",
+      width: "+=15px"
     }).animate({
-        height: '-=15px',
-        width: '-=15px'
+        height: "-=15px",
+        width: "-=15px"
       });
 });
