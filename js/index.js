2ee6f7c5fd0b2d4b2b990ea6a20f79775da1fd71
luciasnow <156396387+luciasnow@users.noreply.github.com>
2024-02-13 00:00:04 -0700

    jscript/copy changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 $(".logo").click (function(){
     $(".logo").animate({
-      height: "+=15px",
-      width: "+=15px"
-    }, 500).animate({
-        height: "-=15px",
-        width: "-=15px"
-      }, 500)
+      height: "-=15px",
+      width: "-=15px"
+    }).animate({
+        height: "+=15px",
+        width: "+=15px"
+      });
 });
