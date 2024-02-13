b357575d4ae51a4f33853fd157f8e85c0693c115
luciasnow <156396387+luciasnow@users.noreply.github.com>
2024-02-12 22:32:02 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+$("#logo").hover (function(){
+    $("#logo").animate({
+      height: '+=15px',
+      width: '+=15px'
+    }).animate({
+        height: '-=15px',
+        width: '-=15px'
+      });
+});
